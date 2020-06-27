4d8f589ef4e93500f8d5f55a293b4276ff7171d8
Lalit chandora <lalit.n.chandora@gmail.com>
2020-06-28 00:09:02 +0530

    Third version

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 from . import views 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('delete/<str:title>',views.deletetask,name="delete"),
+    #path('create/<str:task_title>/<str:task_text>',views.createtask,name="create"),
+    
 ]
--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import CreateTaskForm
 
 def home(request):
-    queryset = Task.objects.all()
-    return render(request,'todo/card.html',{'queryset': queryset})
+    if request.method=="POST":
+        form = CreateTaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CreateTaskForm()
+    queryset = Task.objects.all()[::-1]
+    return render(request,'todo/card.html',{'queryset': queryset,'form':form})
+
+def deletetask(request,title):
+    Task.objects.filter(title=title).delete()
+    return redirect('home')
