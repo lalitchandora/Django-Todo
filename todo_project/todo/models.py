f6f93701f034ee07bcd7e89230a6bccf684763b1
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-06 16:26:03 +0530

    added "date" field to the model

--- todo_project/todo/models.py
+++ todo_project/todo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
