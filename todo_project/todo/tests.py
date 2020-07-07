d533385a581c9b9cf7b4193ebc212d6dc4cd5c7d
Lalit chandora <lalit.n.chandora@gmail.com>
2020-07-07 17:45:35 +0530

    added tests for test_was_added_today

--- todo_project/todo/tests.py
+++ todo_project/todo/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Task
+
+class TaskTests(TestCase):
+    def test_was_added_today(self):
+        date = timezone.now().date()
+
+        task = Task(title = 'sample title',text = 'sample text',date = date)
+        self.assertIs(task.was_added_today(),True)
+
+    
